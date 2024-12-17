0058f7c3978497a027a00e1e5529c2c3ab70557f
Mehebul02 <mehebul2123@gmail.com>
2024-12-17 22:50:39 +0600

    banner 1st section complete

--- src/components/heroSection/Hero.jsx
+++ src/components/heroSection/Hero.jsx
@@ -1,28 +1,39 @@
-import { banking, bannerMen, chart, invertor } from '@/assets/images';
-import Image from 'next/image';
-import React from 'react';
+import { banking, bannerMen, chart, invertor } from "@/assets/images";
+import Image from "next/image";
+import React from "react";
 
 const Hero = () => {
-    return (
-        // main container 
-        <div className='flex justify-between items-center'>
-           {/* 1st section  */}
-           <div className='flex justify-between items-center'>
-          <div>
-            <Image src={banking} alt='banking'/>
-            <Image src={invertor} alt='invertor'/>
-          </div>
-          <div>
-            <Image src={bannerMen} alt='banking'/>
-            <Image src={chart} alt='chart'/>
-          </div>
-           </div>
-           {/* 2nd section  */}
-           <div>
-            2222
-           </div>
+  return (
+    // main container
+    <div className="flex justify-between items-center gap-16">
+      {/* 1st section  */}
+      <div className="w-[50%] flex justify-between items-center">
+        <div>
+          <Image src={banking} alt="banking" />
+          <Image src={invertor} alt="invertor" />
         </div>
-    );
+        <div>
+          <Image src={bannerMen} alt="banking" />
+          <Image src={chart} alt="chart" />
+        </div>
+      </div>
+      {/* 2nd section  */}
+      <div className="w-[50%]">
+        <div>
+        <div className="w-[400px] flex items-center gap-3 rounded-full bg-[#00233F] px-4 py-2">
+          <div className="flex h-10 w-10 items-center justify-center rounded-full bg-[#26F3E7]">
+            <span className="text-[#00233F] font-semibold text-lg">حلال</span>
+          </div>
+          <span className="text-lg font-WorkSanser text-[#28D454]">
+            Islamic Finance, Shari’a Approved
+          </span>
+        </div>
+        </div>
+        <h1 className="text-[60px] text-[#111322] font-WorkSanser font-bold">Smart Investments for Verified SMES</h1>
+       
+      </div>
+    </div>
+  );
 };
 
 export default Hero;
