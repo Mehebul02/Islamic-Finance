22b8ebdb3221054bb368eaacf4777c3d18130c81
Mehebul02 <mehebul2123@gmail.com>
2024-12-19 21:07:36 +0600

    solved the logo slider

--- src/components/feature-slider/FeatureSlide.jsx
+++ src/components/feature-slider/FeatureSlide.jsx
@@ -3,7 +3,7 @@ import LogoSlider from "./Slider";
 const FeatureSlide = () => {
   return (
     <div className="mt-16">
-      <div className="flex items-center gap-2 text-gray-500 text-lg">
+      <div className="flex items-center gap-2 text-gray-500 text-lg p-4">
         <span>Feature in Trusted by</span>
         <span className="font-semibold text-black">→</span>
       </div>
--- src/components/feature-slider/Slider.jsx
+++ src/components/feature-slider/Slider.jsx
@@ -1,66 +1,62 @@
-"use client";
-import { slide1, slide2, slide3, slide4, slide5, slide6, slide7 } from "@/assets/images";
-import dynamic from "next/dynamic";
-import Image from "next/image";
-import 'swiper/css';
-import 'swiper/css/pagination';
-import { Pagination, Autoplay } from 'swiper/modules';
 
-// Dynamically import Swiper components
-const Swiper = dynamic(() => import("swiper/react").then((mod) => mod.Swiper), { ssr: false });
-const SwiperSlide = dynamic(() => import("swiper/react").then((mod) => mod.SwiperSlide), { ssr: false });
+"use client";
+
+import React from "react";
+import { Swiper, SwiperSlide } from "swiper/react";
+import { Navigation, Autoplay } from "swiper/modules";
+import "swiper/css";
+import "swiper/css/navigation";
+import Image from "next/image";
+import Container from "@/shared/Container";
+import {
+  slide1,
+  slide2,
+  slide3,
+  slide4,
+  slide5,
+  slide6,
+} from "@/assets/images";
+
+const logoSlider = [
+  { id: 1, src: slide1 },
+  { id: 2, src: slide2 },
+  { id: 3, src: slide3 },
+  { id: 4, src: slide4 },
+  { id: 5, src: slide5 },
+  { id: 6, src: slide6 },
+];
 
 const LogoSlider = () => {
-    const slides = [
-        { id: 1, src: slide1, alt: "slider1" },
-        { id: 2, src: slide2, alt: "slider2" },
-  
-      ];
   return (
-    <div className="w-full">
-      <Swiper
-        slidesPerView={1} // Show one slide at a time
-        centeredSlides={true}
-        spaceBetween={30} // Space between slides
-        grabCursor={true}
-        pagination={{
-          clickable: true,
-        }}
-        autoplay={{
-          delay: 3000, // Slide changes every 3 seconds
-          disableOnInteraction: false, // Autoplay doesn't stop when interacting with the slider
-        }}
-        modules={[Pagination, Autoplay]} // Add Autoplay module here
-        className="mySwiper"
-      >
-        {/* Each slide contains two images side by side */}
-        {/* Map through slides */}
-    
-        <SwiperSlide>
-          <div className="flex justify-center items-center space-x-4">
-            <div className="flex justify-center items-center">
-              <Image src={slide1} alt="slider1" className="object-contain w-40 h-auto" />
-            </div>
-            <div className="flex justify-center items-center">
-              <Image src={slide2} alt="slider2" className="object-contain w-40 h-auto" />
-            </div>
-            <div className="flex justify-center items-center">
-              <Image src={slide3} alt="slider2" className="object-contain w-40 h-auto" />
-            </div>
-            <div className="flex justify-center items-center">
-              <Image src={slide4} alt="slider2" className="object-contain w-40 h-auto" />
-            </div>
-            <div className="flex justify-center items-center">
-              <Image src={slide5} alt="slider2" className="object-contain w-40 h-auto" />
-            </div>
-            <div className="flex justify-center items-center">
-              <Image src={slide6} alt="slider2" className="object-contain w-40 h-auto" />
-            </div>
-        
-          </div>
-        </SwiperSlide>
-        {/* Add more slides as needed */}
-      </Swiper>
+    <div className="text-white py-12 px-6">
+      <Container>
+        <Swiper
+          modules={[Navigation, Autoplay]}
+          spaceBetween={0} // Reduced gap to 0 for no space between slides
+          slidesPerView={5} // Increased slides for a compact layout
+          loop={true}
+          navigation={false} // Navigation buttons disabled
+          autoplay={{
+            delay: 3000,
+            disableOnInteraction: false,
+          }}
+          breakpoints={{
+            320: { slidesPerView: 2, spaceBetween: 0 },
+            768: { slidesPerView: 3, spaceBetween: 0 },
+            1024: { slidesPerView: 5, spaceBetween: 0 },
+          }}
+        >
+          {logoSlider.map((slide) => (
+            <SwiperSlide key={slide.id} className="flex justify-center items-center">
+              <Image
+                src={slide.src}
+                alt={`slider-${slide.id}`}
+                className="object-contain w-40 h-auto"
+              />
+            </SwiperSlide>
+          ))}
+        </Swiper>
+      </Container>
     </div>
   );
 };
